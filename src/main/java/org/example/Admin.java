b2dd0a2e64677ddfc0d0246e9c7b72bebc0dd4ec
Casper <casper.fischer-gamer@hotmail.com>
2020-04-13 19:44:56 +0200

    First take on User
    Haven't solved the assign variable problem yet

--- src/main/java/org/example/Admin.java
+++ src/main/java/org/example/Admin.java
@@ -1,4 +1,22 @@
 package org.example;
 
-public class Admin {
+public class Admin extends User{
+    public Admin(String name, String email, String password){
+        super(name, email, password);
+    }
+
+    @Override
+    public boolean getIsSuperAdmin() {
+        return false;
+    }
+
+    @Override
+    public boolean getIsAdmin() {
+        return true;
+    }
+
+    @Override
+    public boolean getIsProducer() {
+        return false;
+    }
 }
--- src/main/java/org/example/Producer.java
+++ src/main/java/org/example/Producer.java
@@ -1,5 +1,22 @@
 package org.example;
 
-public class Producer {
-    private int producerId;
+public class Producer extends User{
+    public Producer(String name, String email, String password){
+        super(name, email, password);
+    }
+
+    @Override
+    public boolean getIsSuperAdmin() {
+        return false;
+    }
+
+    @Override
+    public boolean getIsAdmin() {
+        return false;
+    }
+
+    @Override
+    public boolean getIsProducer() {
+        return true;
+    }
 }
