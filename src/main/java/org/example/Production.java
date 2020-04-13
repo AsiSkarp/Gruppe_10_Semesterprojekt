8141372d272d2d51b55936875957acff454e3143
Ahmad-Hammami <ahham19@student.sdu.dk>
2020-04-13 16:35:05 +0200

    Ahmad&Kevin

--- src/main/java/org/example/Production.java
+++ src/main/java/org/example/Production.java
@@ -1,5 +1,20 @@
 package org.example;
 
+import java.util.ArrayList;
+
 public class Production {
     private String title;
+    private int productionId;
+    private ArrayList<CrewMember> productionCrewMList;
+    private Producer producerId;
+
+    public Production(String title, int productionId, ArrayList<CrewMember> productionCrewMList, Producer producerId) {
+        this.title = title;
+        this.productionId = productionId;
+        this.productionCrewMList = productionCrewMList;
+        this.producerId = producerId;
+
+
+
+    }
 }
