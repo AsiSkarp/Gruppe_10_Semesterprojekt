84702d75fe6346cd12f426e5eba7ed7c586ac5dd
Ásbjörn Skarphéðinsson <asiskarp@gmail.com>
2020-04-15 17:30:04 +0200

    Added CreditSystem.java file, without implementation

--- src/main/java/org/example/CreditSystem.java
+++ src/main/java/org/example/CreditSystem.java
@@ -1,5 +1,33 @@
 package org.example;
 
+import java.io.*;
+import java.util.ArrayList;
+
 public class CreditSystem {
+    ArrayList userList = new ArrayList<>();
+    ArrayList productionList = new ArrayList<>();
+    ArrayList personList = new ArrayList();
+
+    //File
+    //BufferedReader
+    FileWriter fileWriter;
+
+    //Read users from CSV File (UNFINISHED)
+    BufferedReader bufferedReader;
+
+    {
+        try {
+            bufferedReader = new BufferedReader(new FileReader("user_list.csv"));
+            String row;
+            while((row = bufferedReader.readLine()) != null) {
+                String[] userData = row.split(",");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
     
 }
+
+
+
